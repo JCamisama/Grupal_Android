de27293873939a0f1d369a4747181c6fdc97e3d0
Camilo Agudelo <jcagudelo001@ikasle.ehu.eus>
2022-05-04 07:59:03 +0200

    DEV_013: Menú implementado

--- app/src/main/java/com/example/grupal_android/MainActivity.java
+++ app/src/main/java/com/example/grupal_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.grupal_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Menú con idioma coming soon!");
+        this.initializeMenuBar();
     }
+
+
+    /**
+     * Se utiliza en todas las actividades herederas para poder inicializar el menú
+     * de la aplicación.
+     */
+    protected void initializeMenuBar() {
+        setSupportActionBar(findViewById(R.id.toolbar));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
