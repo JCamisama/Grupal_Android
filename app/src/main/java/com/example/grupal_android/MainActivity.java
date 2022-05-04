fe7c680c2a26f832115c76f91fc13a0eeb31e340
Camilo Agudelo <jcagudelo001@ikasle.ehu.eus>
2022-05-04 06:48:40 +0200

    DEV_013: Primer commit

--- app/src/main/java/com/example/grupal_android/MainActivity.java
+++ app/src/main/java/com/example/grupal_android/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Menú con idioma coming soon!");
     }
 }
